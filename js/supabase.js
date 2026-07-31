dba8e3347c8b6c19670aa1bc2151eae3b241a8ec
jwanjzks <31046888+jwanjzks@users.noreply.github.com>
2026-07-31 04:22:33 +0200

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://crdyupgkvxxronsjcryl.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_s_y8hb-vPnOfWvopJyzb3Q_HO_HJo3e";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
